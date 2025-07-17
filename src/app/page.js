9a320ea39d5a04664d5891850401588cc15aadc9
AlreadyArio <scrxptedd@gmail.com>
2025-07-17 19:03:48 +0200

    Spell check

--- src/app/page.js
+++ src/app/page.js
@@ -22,17 +22,17 @@ export default function Home() {
             <div className="quote">
               &quot;If you can&apos;t explain something simply you don&apos;t understand it well enough&quot;
             </div>
-            <p>It means that simple comes from understanding <span className="highlight">what is essential</span>. Then complex is not understanding enough to keep it essential. If things are complex you do not understand it enough to make it simple. A simple life does not come from owning few things, it comes from understanding what are the right few necessary things. Ask yourself the most secret and precious question. <span className="highlight">The question of what we truly want</span>.</p>
+            <p>It means that simple comes from understanding <span className="highlight">what is essential</span>. Likewise then complex is not understanding enough of what is essential. So I'm reminded of a quote: "A simple life does not come from owning few things, it comes from understanding what are the right few necessary things. We should stop and ask ourselves the most secret and precious question. <span className="highlight">The question of what we truly want</span>.&quot; Software should understand what it&apos;s users want.</p>
           </div>
 
           <div className="principle">
             <h3>2. Taste what you cook for others</h3>
-            <p>You&apos;ve got to always taste what you cook because how can you cook for others if you&apos;re not confident that what you made is good? How can you share you software with others if you never actually use it yourself to see if it actually is something good? <span className="highlight">Taste what you cook for others</span>.</p>
+            <p>You&apos;ve got to always taste what you cook because how can you cook for others if you&apos;re not confident that what you made is good? How can you share you software with others if you never actually use it yourself to see if it actually is something good? <span className="highlight">Use your own software. Taste your cook. Adjust if needed.</span>.</p>
           </div>
 
           <div className="principle">
             <h3>3. Trying to sell to a country.</h3>
-            <p>Not everybody wants what you have. Not everybody knows what you have in the first place. <span className="highlight">Focus comes from being specific</span> on what you&apos;re trying to do. If you&apos;re not specific you&apos;re focusing on everyone and reaching no one.</p>
+            <p>Not everybody wants what you have. Not everybody knows what you have in the first place. <span className="highlight">Focus comes from being specific</span> on what you&apos;re trying to do. If you&apos;re not specific you&apos;re focusing on everyone and everything and reaching no one. Software should know what it stands for..</p>
           </div>
 
           <div className="principle">
@@ -42,7 +42,7 @@ export default function Home() {
 
           <div className="principle">
             <h3>5. Radio Static</h3>
-            <p>When you turn on the radio you&apos;re mostly likely greeted by the sound of static. <span className="highlight">Noise</span>. The space in between channels. And, when you turn the dial you find that it reaches the right signal, that&apos;s music stations or the news channel. When you have a goal you&apos;ve got to know when you&apos;re getting closer to the <span className="highlight">signal</span> and try to keep yourself on that signal as much as possible. Let&apos;s say your goal is to get three users this month. Why then would you spend 20 days creating the perfect website. (Noise) When you can just as well send out 25 emails in one day and get those three users (Signal). We&apos;ve got to learn, how to ask ourselves:</p>
+            <p>When you turn on the radio you&apos;re most likely greeted by the sound of static. <span className="highlight">Noise</span>. The space in between channels. And, when you turn the dial, you find that it reaches the right signal. That signal being music stations or the news channel. When you have a goal you&apos;ve got to know when you&apos;re getting closer to the <span className="highlight">signal</span> and try to keep yourself on that signal as much as possible. Let&apos;s say your goal is to get three users this month. Why then would you spend 20 days creating the perfect website. (Noise) When you can just as well send out 25 emails in one day and get those three users. (Signal) We&apos;ve got to learn, how to ask ourselves:</p>
             <div className="quote">
               &quot;Will doing this get me to where I want to go?&quot;
             </div>
@@ -51,12 +51,12 @@ export default function Home() {
 
           <div className="principle">
             <h3>6. Cluttered Desk</h3>
-            <p>For as I can remember my room had always been simple, organized and clean. I took inspiration from Goodbye, Things by Fumio Sasaki and that picture from Steve Jobs sitting in his apartment with only some music, papers and a lamp. Until recently when searching the internet, I found another picture from this guy. In his office. With clutter all around. This was when the iPhone and iMac were released and I had a revelation. <span className="highlight">When you have everything out in front of you it is easier to connect dots between concepts and ideas</span>. Software should be the desk of the user.</p>
+            <p>For as long I can remember my room had always been simple, organized and clean. I took inspiration from Goodbye, Things by Fumio Sasaki and that picture from Steve Jobs sitting in his apartment with only some music records, papers and a lamp. Until recently when searching the internet, I found another picture from this guy. In his office. With BANGING clutter all around. This was when the iPhone and iMac were released and I had a revelation. <span className="highlight">When you have everything out in front of you it is easier to connect dots between concepts and ideas</span>. Software should be the desk of the user.</p>
           </div>
 
           <div className="principle">
-            <h3>7.On the Spot</h3>
-            <p>This manifesto was recorded, transcribed, highlighted and now rewritten to a readable manifesto. Before recording I had written down many things that I wanted to talk about and yet when the recording started I discarded the keynotes. I realised that what I would come up with on the spot would be <span className="highlight">one hundred times better</span> than what I&apos;d written down. Looking back, it&apos;s nothing like it and it is also quite similar because my mind remembered what was really important and found out a better way to say it. Software should be the mind which makes our story, the blog, video, music, game or drawing or anything which we create better. <span className="highlight">Software should make your creations better</span>.</p>
+            <h3>7. On the Spot</h3>
+            <p>This manifesto was recorded, transcribed, highlighted and now rewritten to a readable manifesto. Before recording I had written down many things that I wanted to talk about and yet when the recording started I discarded the keynotes. I realized that what I would come up with on the spot would be <span className="highlight">one hundred times better</span> than what I&apos;d written down. Looking back, it&apos;s nothing like it and it is also quite similar because my mind remembered what was really important and found out a better way to say it. <span className="highlight"> Software should be the mind which makes our story, the blog, video, music, game or drawing or anything which we create or do, one hundred time better.</span>.</p>
           </div>
         </div>
       </section>
