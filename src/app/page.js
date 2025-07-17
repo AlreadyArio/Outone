7c7afa3fec15a8e40d3b152f68b8490d0cecd2ab
AlreadyArio <scrxptedd@gmail.com>
2025-07-17 15:40:02 +0200

    better padding

--- src/app/page.js
+++ src/app/page.js
@@ -18,7 +18,7 @@ export default function Home() {
       <section className="manifesto">
         <div className="container">
           <div className="principle">
-            <h3>Simple is understanding.</h3>
+            <h3>1. Simple is understanding.</h3>
             <div className="quote">
               &quot;If you can&apos;t explain something simply you don&apos;t understand it well enough&quot;
             </div>
@@ -26,22 +26,22 @@ export default function Home() {
           </div>
 
           <div className="principle">
-            <h3>Taste what you cook for others</h3>
+            <h3>2. Taste what you cook for others</h3>
             <p>You&apos;ve got to always taste what you cook because how can you cook for others if you&apos;re not confident that what you made is good? How can you share you software with others if you never actually use it yourself to see if it actually is something good? <span className="highlight">Taste what you cook for others</span>.</p>
           </div>
 
           <div className="principle">
-            <h3>Trying to sell to a country.</h3>
+            <h3>3. Trying to sell to a country.</h3>
             <p>Not everybody wants what you have. Not everybody knows what you have in the first place. <span className="highlight">Focus comes from being specific</span> on what you&apos;re trying to do. If you&apos;re not specific you&apos;re focusing on everyone and reaching no one.</p>
           </div>
 
           <div className="principle">
-            <h3>Leave it and just take a picture.</h3>
+            <h3>4. Leave it and just take a picture.</h3>
             <p>We all have moments when we think too much about something or try to do too much to at once. I was reminded of this when preparing to take picture of a laptop I was about to sell online. I turned on all the lights and set up the laptop at the table and as I was about to get the ladder for a perfect top down picture my mother saw me and saved me from myself. She said <span className="highlight">&quot;Leave the ladder, close the door and just take a picture&quot;</span></p>
           </div>
 
           <div className="principle">
-            <h3>Radio Static</h3>
+            <h3>5. Radio Static</h3>
             <p>When you turn on the radio you&apos;re mostly likely greeted by the sound of static. <span className="highlight">Noise</span>. The space in between channels. And, when you turn the dial you find that it reaches the right signal, that&apos;s music stations or the news channel. When you have a goal you&apos;ve got to know when you&apos;re getting closer to the <span className="highlight">signal</span> and try to keep yourself on that signal as much as possible. Let&apos;s say your goal is to get three users this month. Why then would you spend 20 days creating the perfect website. (Noise) When you can just as well send out 25 emails in one day and get those three users (Signal). We&apos;ve got to learn, how to ask ourselves:</p>
             <div className="quote">
               &quot;Will doing this get me to where I want to go?&quot;
@@ -50,12 +50,12 @@ export default function Home() {
           </div>
 
           <div className="principle">
-            <h3>Cluttered Desk</h3>
+            <h3>6. Cluttered Desk</h3>
             <p>For as I can remember my room had always been simple, organized and clean. I took inspiration from Goodbye, Things by Fumio Sasaki and that picture from Steve Jobs sitting in his apartment with only some music, papers and a lamp. Until recently when searching the internet, I found another picture from this guy. In his office. With clutter all around. This was when the iPhone and iMac were released and I had a revelation. <span className="highlight">When you have everything out in front of you it is easier to connect dots between concepts and ideas</span>. Software should be the desk of the user.</p>
           </div>
 
           <div className="principle">
-            <h3>On the Spot</h3>
+            <h3>7.On the Spot</h3>
             <p>This manifesto was recorded, transcribed, highlighted and now rewritten to a readable manifesto. Before recording I had written down many things that I wanted to talk about and yet when the recording started I discarded the keynotes. I realised that what I would come up with on the spot would be <span className="highlight">one hundred times better</span> than what I&apos;d written down. Looking back, it&apos;s nothing like it and it is also quite similar because my mind remembered what was really important and found out a better way to say it. Software should be the mind which makes our story, the blog, video, music, game or drawing or anything which we create better. <span className="highlight">Software should make your creations better</span>.</p>
           </div>
         </div>
