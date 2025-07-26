fa0effc98826eea3b515eb029573484afd0c7c97
Ario <55791489+AlreadyArio@users.noreply.github.com>
2025-07-27 00:27:01 +0200

    Sharper / Simpler

--- src/app/page.js
+++ src/app/page.js
@@ -3,67 +3,61 @@ export default function Home() {
     <>
       <header className="header">
         <div className="container">
-          <h1>The Software Manifesto</h1>
-          <p className="subtitle">My understanding of what truly matters in software.</p>
+          <h1>Ideas</h1>
+          <p className="subtitle"></p>
         </div>
       </header>
 
       <section className="hero">
         <div className="container">
-          <h2>You&apos;re the driver, software is the car.</h2>
-          <p>When you drive a car, you already know your destination. You know where you want to go. To the supermarket. To the music store. To the restaurant for a dinner reservation. You drive the car to the destination and the car gives you everything you need to navigate to it. Software should give you all the tools to get to where you want to go.</p>
+          <h2>PS. Get Real</h2>
+          <p>You already know your destination. All you need is the tools to navigate to it. Software should give you them to get to where you want to go.</p>
         </div>
       </section>
 
       <section className="manifesto">
         <div className="container">
           <div className="principle">
-            <h3>1. Simple is understanding.</h3>
+            <h3>1. What do you want?</h3>
             <div className="quote">
               &quot;If you can&apos;t explain something simply you don&apos;t understand it well enough&quot;
             </div>
-            <p>It means that simple comes from understanding <span className="highlight">what is essential</span>. Likewise then complex is not understanding enough of what is essential. So I&apos;m reminded of a quote: &quot;A simple life does not come from owning few things, it comes from understanding what are the right few necessary things. We should stop and ask ourselves the most secret and precious question. <span className="highlight">The question of what we truly want</span>.&quot; Software should understand what it&apos;s users want.</p>
+            <p>Simple comes from understanding. So I&apos;m reminded of a quote: &quot;A simple life does not come from owning few things, it comes from understanding what are the right few necessary things. We should stop and ask ourselves the most secret and precious question. The question of what we truly want.&quot; Software should understand what it&apos;s users want.</p>
           </div>
 
           <div className="principle">
-            <h3>2. Taste what you cook for others</h3>
-            <p>You&apos;ve got to always taste what you cook because how can you cook for others if you&apos;re not confident that what you made is good? How can you share you software with others if you never actually use it yourself to see if it actually is something good? <span className="highlight">Use your own software. Taste your cook. Adjust if needed.</span>.</p>
+            <h3>2. Give back to others</h3>
+            <p>How can you cook a meal for others if you dont know what you made is good? How can you share you software with others if you never actually use it yourself? Software should be used by those who make it</p>
           </div>
 
           <div className="principle">
-            <h3>3. Trying to sell to a country.</h3>
-            <p>Not everybody wants what you have. Not everybody knows what you have in the first place. <span className="highlight">Focus comes from being specific</span> on what you&apos;re trying to do. If you&apos;re not specific you&apos;re focusing on everyone and everything and reaching no one. Software should know what it stands for..</p>
+            <h3>3. A tnemunom</h3>
+            <p>Focus comes from being specific on what you&apos;re trying to do. Software should know what it stands for..</p>
           </div>
 
           <div className="principle">
-            <h3>4. Leave it and just take a picture.</h3>
-            <p>We all have moments when we think too much about something or try to do too much to at once. I was reminded of this when preparing to take a picture of a laptop I was about to sell online. I turned on all the lights and set up the laptop at the table and as I was about to get the ladder for a perfect top down picture my mother saw me and saved me from myself. She said <span className="highlight">&quot;Leave the ladder, close the door and just take a picture&quot;</span></p>
+            <h3>4. Just take a picture</h3>
+            <p>When preparing to take a picture of a laptop I was about to sell online. I was about to get the ladder for a perfect top down picture my mother saw me. She said <span className="highlight">&quot;Leave the ladder, close the door and just take a picture&quot;</span></p>
           </div>
 
           <div className="principle">
-            <h3>5. Radio Static</h3>
-            <p>When you turn on the radio you&apos;re most likely greeted by the sound of static. <span className="highlight">Noise</span>. The space in between channels. And, when you turn the dial, you find that it reaches a signal. That signal being the music stations or the news channel. When you have a goal you&apos;ve got to know when you&apos;re getting closer to the <span className="highlight">signal</span> and try to keep yourself on that signal as much as possible. Let&apos;s say your goal is to get three users this month. Why then would you spend 20 days creating the perfect website. (Noise) When you can just as well send out 25 emails in one day and get those three users. (Signal) We&apos;ve got to learn, how to ask ourselves:</p>
+            <h3>5. STATIC</h3>
+            <p>Turn on the radio and you&apos;re most likely greeted by static sound, the noise. When you turn the dial, you find that it reaches the music or news stations. When you have a goal keep yourself on that signal as much as possible. Do you need that perfect picture or is a good one enough? We&apos;ve got to constantly ask ourselves:</p>
             <div className="quote">
               &quot;Will doing this get me to where I want to go?&quot;
             </div>
-            <p>After six months I figured out how to do it. Be aware.</p>
           </div>
 
           <div className="principle">
-            <h3>6. Cluttered Desk</h3>
-            <p>For as long I can remember my room had always been simple, organized and clean. I took inspiration from Goodbye, Things by Fumio Sasaki and that picture from Steve Jobs sitting in his apartment with only some music records, papers and a lamp. Until recently when searching the internet, I found another picture from this guy. In his office. With BANGING clutter all around. This was when the iPhone and iMac were released and I had a revelation. <span className="highlight">When you have everything out in front of you it is easier to connect dots between concepts and ideas</span>. Software should be the desk of the user.</p>
+            <h3>6. Messy Steve</h3>
+            <p>My room had always been simple and organized. I took inspiration from that picture from Steve Jobs sitting in his apartment with only some music records, papers and a lamp. Recently I found another picture. In his office. With clutter all around. I realised when you have everything out in front of you it is easier to connect between concepts and ideas. Software should be the desk of the user.</p>
           </div>
 
-          <div className="principle">
-            <h3>7. On the Spot</h3>
-            <p>This manifesto was recorded, transcribed, highlighted and now rewritten to a readable manifesto. Before recording I had written down many things that I wanted to talk about and yet when the recording started I discarded the keynotes. I realized that what I would come up with on the spot would be <span className="highlight">one hundred times better</span> than what I&apos;d written down. Looking back, it&apos;s nothing like it and it is also quite similar because my mind remembered what was really important and found out a better way to say it. <span className="highlight"> Software should be the mind which makes our story, the blog, video, music, game or drawing or anything which we create or do, one hundred times better.</span>.</p>
-          </div>
-        </div>
       </section>
 
       <footer>
         <div className="container">
-          <p>Written by Arion Volkerts</p>
+          <p></p>
         </div>
       </footer>
     </>
