e58d62e0d9be7ced8ad729181544507c516fec58
Ario <55791489+AlreadyArio@users.noreply.github.com>
2025-07-31 00:03:42 +0200

    simpler ideas

--- src/app/page.js
+++ src/app/page.js
@@ -3,15 +3,14 @@ export default function Home() {
     <>
       <header className="header">
         <div className="container">
-          <h1>Ideas</h1>
+          <h1>Break Out One</h1>
           <p className="subtitle"></p>
         </div>
       </header>
 
       <section className="hero">
         <div className="container">
-          <h2>PS. Get Real</h2>
-          <p>You already know your destination. All you need is the tools to navigate to it. Software should give you them to get to where you want to go.</p>
+          <p>You already know your destination. You need the tools to navigate to it. Software should give you those tools to get to your destination.</p>
         </div>
       </section>
 
@@ -20,37 +19,24 @@ export default function Home() {
           <div className="principle">
             <h3>1. What do you want?</h3>
             <div className="quote">
-              &quot;If you can&apos;t explain something simply you don&apos;t understand it well enough&quot;
+              &quot;Simple means understanding&quot;
             </div>
-            <p>Simple comes from understanding. So I&apos;m reminded of a quote: &quot;A simple life does not come from owning few things, it comes from understanding what are the right few necessary things. We should stop and ask ourselves the most secret and precious question. The question of what we truly want.&quot; Software should understand what it&apos;s users want.</p>
+            <p>So I&apos;m reminded of a quote: &quot;A simple life does not come from owning few things, it comes from understanding what are the right few necessary things. We should stop and ask ourselves the most secret and precious question. The question of what we truly want.&quot; Software should understand what it&apos;s users want.</p>
           </div>
 
           <div className="principle">
             <h3>2. Give back to others</h3>
-            <p>How can you cook a meal for others if you dont know what you made is good? How can you share you software with others if you never actually use it yourself? Software should be used by those who make it</p>
+            <p>How can you share you software with others if you never actually use it yourself? Software should be used by those who make it</p>
           </div>
 
           <div className="principle">
-            <h3>3. A tnemunom</h3>
-            <p>Focus comes from being specific on what you&apos;re trying to do. Software should know what it stands for..</p>
+            <h3>3. Just take a picture</h3>
+            <p>When preparing to sell my laptop, I was busy taking out the ladder for a perfect top down picture. Thats when someone saw me and said <span className="highlight">&quot;Leave the ladder, close the door and just take a picture&quot;</span>. Don't go off course on your journey towards your destination. Software should know what it stands for.</p>
           </div>
 
           <div className="principle">
-            <h3>4. Just take a picture</h3>
-            <p>When preparing to take a picture of a laptop I was about to sell online. I was about to get the ladder for a perfect top down picture my mother saw me. She said <span className="highlight">&quot;Leave the ladder, close the door and just take a picture&quot;</span></p>
-          </div>
-
-          <div className="principle">
-            <h3>5. STATIC</h3>
-            <p>Turn on the radio and you&apos;re most likely greeted by static sound, the noise. When you turn the dial, you find that it reaches the music or news stations. When you have a goal keep yourself on that signal as much as possible. Do you need that perfect picture or is a good one enough? We&apos;ve got to constantly ask ourselves:</p>
-            <div className="quote">
-              &quot;Will doing this get me to where I want to go?&quot;
-            </div>
-          </div>
-
-          <div className="principle">
-            <h3>6. Messy Steve</h3>
-            <p>My room had always been simple and organized. I took inspiration from that picture from Steve Jobs sitting in his apartment with only some music records, papers and a lamp. Recently I found another picture. In his office. With clutter all around. I realised when you have everything out in front of you it is easier to connect between concepts and ideas. Software should be the desk of the user.</p>
+            <h3>4. Messy Desk</h3>
+            <p>When you have everything out in front of you it is easier to connect between concepts and ideas. Software should be the desk of the user.</p>
           </div>
         </div>
       </section>
