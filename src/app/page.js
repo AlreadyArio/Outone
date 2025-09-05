d0cab8a436242b585dca648e5b49947ad5f1cc2c
Ario <55791489+AlreadyArio@users.noreply.github.com>
2025-09-06 00:21:23 +0200

    Update page.js revert

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,10 @@ export default function Home() {
       <header className="header">
         <div className="container">
           <h1>The world is yours.</h1>
+<<<<<<< HEAD
           <h1>Wonder.</h1>
+=======
+>>>>>>> 670c4f6 (Update page.js)
           <p className="subtitle"></p>
         </div>
       </header>
