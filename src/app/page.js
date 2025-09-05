da5a28e63d9299c8128131379cdd790882294b86
Ario <55791489+AlreadyArio@users.noreply.github.com>
2025-09-05 23:18:46 +0200

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -9,31 +9,9 @@ export default function Home() {
         </div>
       </header>
 
-      <section className="manifesto">
-        <div className="container">
-          <div className="principle">
-            <h3>1. Simplify</h3>
-            <p>You are complicated.
-You use ten words when you can use one.
-You use one word when you can show an image.</p>
-          </div>
-
-          <div className="principle">
-            <h3>2. Messy</h3>
-            <p>Creativity comes from messy, in the box thinking comes from tidy.
-Tidy makes us care more about others, messy makes us care more about our craft.
-Messy break rules, tidy sticks to rules.
-Tidy and messy are life.
-It goes everywhere we do.</p>
           </div>
         </div>
       </section>
-
-      <footer>
-        <div className="container">
-          <p></p>
-        </div>
-      </footer>
     </>
   )
 }
