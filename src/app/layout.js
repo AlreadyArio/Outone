85c1955e8f9066ebda3a039eba4b5012b021d6fd
Ario <55791489+AlreadyArio@users.noreply.github.com>
2025-09-05 23:19:43 +0200

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,8 @@
 import './globals.css'
 
 export const metadata = {
-  title: 'Outone',
-  description: 'Encouraging simplicity and focus in software design',
+  title: '01',
+  description: '',
 }
 
 export default function RootLayout({ children }) {
